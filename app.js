4a98b91d6ed43d779fc4f80e592b28414c562967
James Hartshorn <jhartshorn@gmail.com>
2017-05-06 20:04:37 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+})); 
+//app.use(express.urlencoded());
+app.use(express.static('public'));
+
+app.post('/handler', function (req, res){
+  if (req.body.message == "liz")
+    res.send('Hello  '  + req.body.message + ' xxx');
+  else
+    res.send('Hello  '  + req.body.message);
+
+  console.log(req.body.message);
+});
+
+app.listen(8000, function () {
+  console.log('App listening on port 8000');
+});
+
